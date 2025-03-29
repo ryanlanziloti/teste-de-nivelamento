fbf88ab844aa0c1e7318eb9c04d311e6d00fbf3c
ryanlanziloti <pitagoraslanz@gmail.com>
2025-03-29 13:01:49 -0300

    att

--- 4 - Teste de API/front_operadoras_ativas/src/App.vue
+++ 4 - Teste de API/front_operadoras_ativas/src/App.vue
@@ -1,20 +1,28 @@
 <script setup>
-import DataTable from './components/DataTable.vue'
-import { onMounted,ref } from 'vue'
+import DataTable from './components/DataTable.vue';
+import Search from './components/Search.vue';
+import { onMounted, ref, watch } from 'vue';
 
-const data = ref([])
-onMounted(async () => {
-  const response = await fetch('http://127.0.0.1:5001/dados') 
-  data.value = await response.json()
-})
+const data = ref([]);
+const searchQuery = ref('');
 
+const fetchData = async (query = '') => {
+  const response = await fetch(`http://127.0.0.1:5000/buscar?q=${query}`);
+  data.value = await response.json();
+};
+
+onMounted(() => fetchData());
+
+watch(searchQuery, (newQuery) => {
+  fetchData(newQuery);
+});
 </script>
 
 <template>
   <h1 class="p-8 bg-gray-100 min-h-screen">
-    <DataTable :data="data" /> 
+    <div class="flex items-center justify-between">
+      <Search @update:search="searchQuery = $event" />
+    </div>
+    <DataTable :items="data" />
   </h1>
 </template>
-
-<style scoped> 
-</style>
---
+++ 4 - Teste de API/front_operadoras_ativas/src/components/Search.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const searchQuery = ref('');
+const emit = defineEmits(['update:search']);
+
+const updateSearch = () => {
+  emit('update:search', searchQuery.value);
+};
+</script>
+
+<template>
+  <input
+    v-model="searchQuery"
+    @input="updateSearch"
+    class="p-2 border rounded"
+    placeholder="Digite sua busca..."
+  />
+</template>
